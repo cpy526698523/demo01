72784279681815304c9529f8764a2c9d5013fa91
cpy5266985223 <526698523@qq.com>
2019-07-02 14:36:04 +0800

    初次提交

---
+++ demo01/src/main/Java/Servlet/MyServlet.java
@@ -0,0 +1,34 @@
+package Servlet;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+
+@WebServlet("/demo")
+public class MyServlet implements Servlet {
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
